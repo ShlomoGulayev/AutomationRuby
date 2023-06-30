ba4b2d7248f37df7f16da4847b73f8d3d00431e5
ShlomoGulayev <shlomo.gulayev1998@gmail.com>
2023-06-30 11:53:39 +0300

    test

---
+++ features/step_definitions/first_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I click on Got it button$/) do
+  puts("Clicked Got it button")
+end
+
+And(/^I see Sq Kilometre in From header$/) do
+  puts("Saw Sq Km in From header")
+end
+
+And(/^I see Sq Metre in To header$/) do
+  puts("Saw Sq Mtr in To header")
+end
